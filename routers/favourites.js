c64a054751980268bbb9ad18ede205bee41d25d3
nmegrant <nme.grant@gmail.com>
2020-08-23 19:07:31 +0200

    Create endpoint for fetching favourites

---
+++ routers/favourites.js
@@ -0,0 +1,21 @@
+const Router = require("express");
+const { toJWT, toData } = require("../auth/jwt");
+const authMiddleware = require("../auth/middleware");
+const User = require("../models").user;
+const Favourites = require("../models").favourite;
+
+const router = new Router();
+
+router.get("/favourites", async (request, response) => {
+  try {
+    const favs = await Favourites.findAll({ where: { userId: 1 } });
+    if (!favs) {
+      return response.status(404).send("No favourites found");
+    }
+    return response.status(200).send([...favs]);
+  } catch (error) {
+    console.log(`Getter user favourites: ${error}`);
+  }
+});
+
+module.exports = router;
